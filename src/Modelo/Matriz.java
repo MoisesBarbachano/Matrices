47b627407d44ddff548bcba85438c7a5500bc6a9
Moisés Barbachano <moises.barbachano.chiu@gmail.com>
2018-09-14 17:33:37 -0500

    Iniciacion

---
+++ src/Modelo/Matriz.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author sulak
+ */
+public class Matriz {
+    int filas;
+    int columnas;
+
+    public Matriz(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+    
+    
+}
