fa55cc435faca80f4a5b6ed7f46651bab27188f0
pepe_ <pepe_@LAPTOP-94N0S5PG>
2018-09-17 17:43:00 -0500

    Agregue la suma, multiplicacion por escalar, multiplicacion de matrices, soy una riata

--- src/Modelo/Operaciones.java
+++ src/Modelo/Operaciones.java
@@ -16,4 +16,39 @@ public class Operaciones {
         
         
     }
+    
+    public double[][] sumar(double[][] primeraMatriz, double[][] segundaMatriz){
+       
+        double terceraMatriz[][] = new double [primeraMatriz[0].length][primeraMatriz.length];
+        for (int fila = 0; fila < primeraMatriz[0].length; fila++) {
+            for (int columna = 0; columna < primeraMatriz.length; columna++) {
+                terceraMatriz [fila][columna] = primeraMatriz [fila][columna] + segundaMatriz [fila][columna];
+            }
+        }
+        return terceraMatriz;
+    }
+    
+    public double[][] multiplicacionEscalar(double[][] matriz, double escalar){
+        
+        for (int fila = 0; fila < matriz[0].length; fila++) {
+            for (int columna = 0; columna < matriz.length; columna++) {
+                matriz [fila][columna] = escalar * (matriz[fila][columna]);
+            }
+        }
+        return matriz;
+    }
+    
+    public double[][] multiplicar(double[][] primeraMatriz, double[][] segundaMatriz){
+        double terceraMatriz[][] = new double [primeraMatriz[0].length][segundaMatriz[0].length];
+        
+        for (int filaPrimeraMatriz = 0; filaPrimeraMatriz < primeraMatriz[0].length; filaPrimeraMatriz++) {
+            for (int columna = 0; columna < primeraMatriz.length; columna++) {
+                for (int columnaSegundaMatriz = 0; columnaSegundaMatriz < segundaMatriz.length; columnaSegundaMatriz++) {
+                    terceraMatriz[filaPrimeraMatriz][columnaSegundaMatriz] += primeraMatriz[filaPrimeraMatriz][columna] * segundaMatriz[columna][columnaSegundaMatriz];
+                }
+            }
+        }
+        return terceraMatriz;
+    }
+    
 }
